570a6b78007551de8c0fbf339139a3fa0ed75a23
ritsuke <ritsuke@gmail.com>
2021-02-03 13:58:54 -0800

    Change to Sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Router from './routes'
 
-import './App.global.css'
+import './App.global.scss'
 
 export default () => {
   return <Router />
--- src/routes/Dashboard/index.tsx
+++ src/routes/Dashboard/index.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 import ProfileList from '../../components/ProfileList'
 
+import styles from './styles.scss'
+
 export default () => {
   return (
     <div>
       <h1>Elm Story</h1>
-      <ProfileList />
+      <ProfileList className={styles.profiles} />
     </div>
   )
 }
