d825344748ee78234f464fd5b332919b89c0495e
ritsuke <ritsuke@gmail.com>
2021-02-19 17:04:32 -0800

    Use HasRouter to fix reload crash

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { HashRouter as Router, Switch, Route, Link } from 'react-router-dom'
 
 import styles from './styles.module.scss'
 
