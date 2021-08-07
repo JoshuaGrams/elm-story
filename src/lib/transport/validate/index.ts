ffe245d367fbca9cfe37e7c4ac641ba774a703df
ritsuke <ritsuke@gmail.com>
2021-08-07 13:03:36 -0500

    #220; new method for validating incoming data

---
+++ src/lib/transport/validate/index.ts
@@ -0,0 +1,28 @@
+import { Validator, Schema, ValidationError } from 'jsonschema'
+
+import { GameDataJSON as GameDataJSON_013 } from '../types/0.1.3'
+import { GameDataJSON as GameDataJSON_020 } from '../types/0.2.0'
+
+function isValidData(data: any, schema: Schema): [boolean, ValidationError[]] {
+  const { errors } = new Validator().validate(data, schema)
+
+  return errors.length === 0 ? [true, []] : [false, errors]
+}
+
+export default (
+  gameData: GameDataJSON_013 | GameDataJSON_020,
+  version: string
+): [boolean, ValidationError[] | { path?: string; message: string }[]] => {
+  try {
+    return isValidData(gameData, require(`../schema/${version}.json`))
+  } catch (error) {
+    return [
+      false,
+      [
+        {
+          message: `Unable to validate game data. Schema '${version}' is not supported.`
+        }
+      ]
+    ]
+  }
+}
