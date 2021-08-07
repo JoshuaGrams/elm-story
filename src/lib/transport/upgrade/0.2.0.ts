5add55d3522860c50bff25450f5c2e4e65dc44a6
ritsuke <ritsuke@gmail.com>
2021-08-07 13:04:16 -0500

    #220; upgrade 0.1.3 to 0.2.0 data on import

---
+++ src/lib/transport/upgrade/0.2.0.ts
@@ -0,0 +1,97 @@
+// upgrades 0.1.3 data to 0.2.0
+import { cloneDeep, pick } from 'lodash-es'
+
+import { COMPONENT_TYPE } from '../../../data/types'
+import { GameDataJSON as GameDataJSON_013 } from '../types/0.1.3'
+import {
+  FolderCollection,
+  GameDataJSON as GameDataJSON_020,
+  SceneCollection
+} from '../types/0.2.0'
+
+export default ({
+  _,
+  chapters,
+  choices,
+  conditions,
+  effects,
+  jumps,
+  passages,
+  routes,
+  scenes,
+  variables
+}: GameDataJSON_013): GameDataJSON_020 => {
+  const clonedChapters = cloneDeep(chapters),
+    folders: FolderCollection = {}
+
+  Object.keys(clonedChapters).map((chapterId) => {
+    folders[chapterId] = {
+      children: clonedChapters[chapterId].scenes.map((sceneId) => [
+        COMPONENT_TYPE.SCENE,
+        sceneId
+      ]),
+      parent: [COMPONENT_TYPE.GAME, null],
+      ...pick(clonedChapters[chapterId], ['id', 'tags', 'title', 'updated'])
+    }
+  })
+
+  const upgradedJumps = cloneDeep(jumps)
+
+  Object.keys(upgradedJumps).map((jumpId) =>
+    upgradedJumps[jumpId].route.shift()
+  )
+
+  const clonedScenes = cloneDeep(scenes),
+    upgradedScenes: SceneCollection = {}
+
+  Object.keys(clonedScenes).map((sceneId) => {
+    const clonedScene = clonedScenes[sceneId]
+
+    upgradedScenes[sceneId] = {
+      children: clonedScene.passages.map((passageId) => [
+        COMPONENT_TYPE.PASSAGE,
+        passageId
+      ]),
+      parent: [COMPONENT_TYPE.FOLDER, clonedScene.chapterId],
+      ...pick(clonedScene, [
+        'editor',
+        'id',
+        'jumps',
+        'tags',
+        'title',
+        'updated'
+      ])
+    }
+  })
+
+  return {
+    _: {
+      children: _.chapters.map((chapterId) => [
+        COMPONENT_TYPE.FOLDER,
+        chapterId
+      ]),
+      engine: '0.2.0',
+      ...pick(_, [
+        'designer',
+        'id',
+        'jump',
+        'schema',
+        'studioId',
+        'studioTitle',
+        'tags',
+        'title',
+        'updated',
+        'version'
+      ])
+    },
+    choices,
+    conditions,
+    effects,
+    folders,
+    jumps: upgradedJumps,
+    passages,
+    routes,
+    scenes: upgradedScenes,
+    variables
+  }
+}
